4d7c313f778d842405778ce9576912abd8f1e15c
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-11-05 23:52:50 +0900

    드디어 첫 Spring 프로젝트 생성 H2, lombok, spring-web, JPA 추가되어있음

---
+++ src/main/java/com/example/springtuto1/Springtuto1Application.java
@@ -0,0 +1,13 @@
+package com.example.springtuto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtuto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtuto1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtuto1/Springtuto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtuto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springtuto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
