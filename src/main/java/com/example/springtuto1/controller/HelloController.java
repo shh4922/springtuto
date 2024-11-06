91a9517a091b18d89d129348f0ea9a28d69f9e4a
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2024-11-06 23:53:02 +0900

    이야 드디어 api를 열어봄. 김영한 강의보면써 따라치면서 스프링구조 익히는중임

---
+++ src/main/java/com/example/springtuto1/controller/HelloController.java
@@ -0,0 +1,43 @@
+package com.example.springtuto1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 응답바디에 return값을 넣어주겠다 라는뜻
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // 응답바디에 return값을 넣어주겠다 라는뜻
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return  hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
